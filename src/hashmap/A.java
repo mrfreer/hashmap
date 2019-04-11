65b46b76388a0d3f9fb6c5e7b625f6a9e60d3621
Kentec <Kentec@2130-INS-343595.mykendall.mymdc.net>
2019-04-11 11:53:08 -0400

    test

---
+++ src/hashmap/A.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Kentec
+ */
+public class A {
+    
+    public static void main(String[] args) {
+        Map map = new HashMap<String, String>();
+        
+    }
+    
+}
